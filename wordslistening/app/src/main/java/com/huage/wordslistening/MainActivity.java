b94e47375e709d31246b79e5428a973923c1c90b
HuaGe <huage19970513@gmail.com>
2020-03-17 09:55:44 +0800

    all function works but exist a little bugs

--- wordslistening/app/src/main/java/com/huage/wordslistening/MainActivity.java
+++ wordslistening/app/src/main/java/com/huage/wordslistening/MainActivity.java
@@ -1,205 +1,126 @@
 package com.huage.wordslistening;
 
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Context;
-import android.content.res.AssetManager;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Color;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.os.Build;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.util.regex.Pattern;
+
+import static java.lang.Integer.parseInt;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity  {
 
-    private Context mContext;
-    private Button btn_back;
-    private Button btn_play;
-    private Button btn_home;
-    private TextView tv_word;
-    private TextView tv_means;
-    private SQLiteDatabase db;
-    private String wordInfo;
-    private final String DATABASE_FILENAME = "words.db";//数据库文件名称
+    private EditText ed_unit;
+    private RadioGroup timegroup;
+    private RadioGroup periodgroup;
+    private TextView tv_load;
+    private int unitNo = 1;
     private int playTime = 1;
-    private int playnum = 0;
-    private MediaPlayer mPlayer = new MediaPlayer();
-    private final String SPEAK_API = "http://dict.youdao.com/dictvoice?type=1&audio=";//有道英文朗读接口
-    private int id;
-    private String word;
-    private String means;
-    private int model = 1;
-
-
+    private int playPeriod = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mContext = MainActivity.this;
-        db = openDatabase();//打开数据库，获得数据库对象
         bindViews();
-    }
-    private SQLiteDatabase openDatabase()
-    {
-
-        // 获取输出流,文件存储目录:data/data/包名/files目录下，文件名相同
-        File file = new File(getFilesDir(),DATABASE_FILENAME);
-
-        // 如果在/sdcard/files目录中不存在xxxx.db文件，则从assets目录中复制这个文件到SD卡的目录（/sdcard/files）
-        /**
-         * 通过输入流和输出流来实现文件的复制（这是最常用的复制文件的方法）
-         */
-        if (!file.exists())
-            {
-                AssetManager assetManager = getAssets();
-                try {
-                    // 获得输入流
-                    InputStream is = assetManager.open(DATABASE_FILENAME);
-                    FileOutputStream fos = new FileOutputStream(file);
-                    //开始读写
-                    byte[] buffer = new byte[8192];
-                    int count = 0;
-                    // 开始复制dictionary.db文件
-                    while ((count = is.read(buffer)) > 0)
-                    {
-                        fos.write(buffer, 0, count);
-                    }
-                    is.close();
-                    fos.close();
-                }catch (Exception e){
-                    Log.e("读取数据库失败",e.getMessage());
-                }
-            }
-
-        // 打开/sdcard/files 目录中的xxxx.db文件
-        SQLiteDatabase database = SQLiteDatabase.openOrCreateDatabase(
-                    getFilesDir()+"/"+DATABASE_FILENAME, null);
-        return database;
-
-
-    }
-    private void bindViews() {
-        btn_back =  findViewById(R.id.btn_back);
-        btn_play  =  findViewById(R.id.btn_play);
-        btn_home  =  findViewById(R.id.btn_home);
-        tv_word   =  findViewById(R.id.tv_word);
-        tv_means  =  findViewById(R.id.tv_means);
-
-        btn_back.setOnClickListener(this);
-        btn_play.setOnClickListener(this);
-        btn_home.setOnClickListener(this);
-        tv_word.setOnClickListener(this);
-        tv_means.setOnClickListener(this);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_back:
-                playTime = 1;
-                playnum = 0;
-                atuoplay();
-                break;
-            case R.id.btn_play:
-                playnum += 1;
-                if(playnum%2!=0){  //播放
-                   atuoplay();
-                }else{ //暂停
-                    btn_play.setBackground(getResources().getDrawable(R.drawable.play));
-                    mPlayer.pause();
-            }
-
-                break;
-            case R.id.btn_home:
-                    mPlayer.stop();
-                    playTime = 1;
-                    playnum = 0;
-                    btn_play.setBackground(getResources().getDrawable(R.drawable.play));
-                break;
-            case R.id.tv_word:
-            case R.id.tv_means:
-                model *= -1;
-                if(model>0){
-                    tv_word.setTextColor(0xF5F0C7);
-                    tv_means.setTextColor(0xF5F0C7);
-                    Toast.makeText(mContext, "默写模式开启", Toast.LENGTH_SHORT).show();
-                }else {
-                    tv_word.setTextColor(Color.BLACK);
-                    tv_means.setTextColor(Color.BLACK);
-                    Toast.makeText(mContext, "默写模式关闭", Toast.LENGTH_SHORT).show();
-                }
-
-                break;
         }
-    }
-    private void atuoplay(){
-        playActivity();
-        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                playTime += 1;
-                Log.v("播放监听程序",playTime+"");
-                playActivity();
-            }
-        });
-    }
-    private void playActivity(){
-        btn_play.setBackground(getResources().getDrawable(R.drawable.pause));
-        String sql = "select * from unit01 where id=?";
-        Cursor cursor = db.rawQuery(sql,new String[]{playTime+""});
+        private void bindViews(){
+            ed_unit =  findViewById(R.id.ed_unit);
+            timegroup =  findViewById(R.id.TimeGroup);
+            periodgroup =  findViewById(R.id.PeriodGroup);
+            tv_load  =  findViewById(R.id.tv_load);
 
-        //  如果查找单词，显示其中文的意思
-        if (cursor.moveToFirst()) {
+            ed_unit. addTextChangedListener(new TextWatcher() {
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                }
 
-                id = cursor.getInt(cursor.getColumnIndex("id"));
-                word = cursor.getString(cursor.getColumnIndex("word")).replace("-"," ");
-                means = cursor.getString(cursor.getColumnIndex("means"));
-                wordInfo = "第" + id + "个单词： " + word + "："+ means;
-                Log.v("数据库查询：",wordInfo);
-                tv_word.setText(word);
-                tv_means.setText(means);
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
 
-                //开始播放
-                try {
-                    mPlayer.reset();
-                    mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-                    mPlayer.setDataSource(SPEAK_API+ word);
-                    mPlayer.prepareAsync();
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                Thread.sleep(2000);
-                            } catch (InterruptedException e) {
-                                Log.e("休眠程序",e.getMessage());
-                            }
-                            mPlayer.start();
+                @Override
+                public void afterTextChanged(Editable s) {
+                    Pattern pattern = Pattern.compile("^[1-9][0-9]*|");
+                    if( pattern.matcher(s).matches()){
+                        if(s==null||(s+"").equals("")){
+                            Log.v("提示","编辑框内容为空");
+                            unitNo = 1;
+                        }else if(parseInt(s+"")<31){
+                            unitNo = parseInt(s+"");
+                            Log.v("播放单元",unitNo+"");
+                        }else {
+                            new AlertDialog.Builder(MainActivity.this)
+                                    .setTitle("选择单元")
+                                    .setMessage("请选择正确的单元(1~30)")
+                                    .setPositiveButton("重新输入", null)
+                                    .show();
+                            ed_unit.setText("1");
                         }
-                    }).start();
-
-                }catch (Exception e){
-                    Log.e("播放失败",e.getMessage());
+                    }else {
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("选择单元")
+                                .setMessage("请选择正确的单元(1~30)")
+                                .setPositiveButton("重新输入", null)
+                                .show();
+                        ed_unit.setText("1");
+                    }
                 }
-
-        }else {
-            new AlertDialog.Builder(this).setTitle("提醒").setMessage("播放完毕").setPositiveButton("确定", null).show();
-            playTime = 1;
+            });
+            timegroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+                    RadioButton btnTime =  findViewById(checkedId);
+                    playTime =parseInt(btnTime.getText()+"");
+                    Log.v("单词播放次数",playTime+"");
+                }
+            });
+            periodgroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+                    RadioButton btnPeriod =  findViewById(checkedId);
+                    playPeriod = parseInt((btnPeriod.getText()+"").substring(0,1));
+                    Log.v("播放间隔",playPeriod+"");
+                }
+            });
+            tv_load.setOnLongClickListener(fingerLongClickListener);
+            tv_load.setOnClickListener(fingerClickListener);
         }
-        cursor.close();
-    }
+    private View.OnLongClickListener fingerLongClickListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            Intent intent = new Intent(MainActivity.this, WorkActivity.class);
+            intent.putExtra("unitNo",unitNo);
+            intent.putExtra("playTime",playTime);
+            intent.putExtra("playPeriod",playPeriod);
+            startActivityForResult(intent,1);
+            finish();
+            return true;
+        }
+
+    };
+
+    private View.OnClickListener fingerClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast toast = Toast.makeText(MainActivity.this, "指纹识别失败，请重试", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.CENTER_VERTICAL,0,-120);
+            toast.show();
+        }
+
+    };
 }
+
